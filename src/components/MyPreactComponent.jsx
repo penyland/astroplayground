b97b9b55277a6a580be19a2fbcfeeeaabec3351d
Peter Nylander <peter.nylander@gmail.com>
2025-02-24 18:08:05 +0100

    Add dynamic time display and layout enhancements with global styles

--- src/components/MyPreactComponent.jsx
+++ src/components/MyPreactComponent.jsx
@@ -1,9 +1,21 @@
 import { useState } from 'preact/hooks';
 
 export default function MyPreactComponent() {
+
+  let time = new Date().toLocaleTimeString();
+  const [ctime, setTime] = useState(time);
+
+  const updateTime = () => {
+    time = new Date().toLocaleTimeString();
+    setTime(time);
+  }
+
+  setInterval(updateTime, 1000);
+
   return (
     <div>
-      <h1>My Preact Component</h1>
+      <h1 class="text-5xl">My Preact Component</h1>
+      <h1 class="text-4xl">{ ctime }</h1>
     </div>
   );
 }
